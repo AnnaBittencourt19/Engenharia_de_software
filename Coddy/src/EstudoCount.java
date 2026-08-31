9d912a4c6b054341b61ea6ce41af96e9df917149
AnnaBittencourt19 <bittencourtanna19@gmail.com>
2026-08-31 20:04:31 -0300

    Coddy

---
+++ Coddy/src/EstudoCount.java
@@ -0,0 +1,16 @@
+public class EstudoCount {
+    public static void main(String[] args) {
+        int count = 0;
+
+        // Digite seu código abaixo
+        count++;
+        count++;
+        count++;
+        count++;
+        count = count *2;
+        count--;
+
+        // Não altere a linha abaixo
+        System.out.println("count = " + count);
+    }
+}
---
+++ Coddy/src/SimplificandoOperacoes.java
@@ -0,0 +1,13 @@
+public class SimplificandoOperacoes {
+    public static void main(String[] args) {
+        int count = 0;
+
+        // Digite seu código abaixo
+        count += 4;
+        count *= 2;
+        count -= 1;
+
+        // Não altere a linha abaixo
+        System.out.println("count = " + count);
+    }
+}
